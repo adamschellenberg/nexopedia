8112cbece44f9ff13d26aae3e8019bd6c7c45f5a
Adam Schellenberg <adamschellenberg93@gmail.com>
2024-01-02 18:29:43 -0700

    working on nexomon page

--- src/components/NexomonPage/NexomonPage.js
+++ src/components/NexomonPage/NexomonPage.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import './NexomonPage.css';
 import { useGetData } from '../../custom-hooks';
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export const NexomonPage = () => {
     const [nexomonName, setNexomonName] = useState("");
@@ -32,9 +34,21 @@ export const NexomonPage = () => {
         <div>
             <div className="nexomonBackground" />
             <Container>
-                <h1 className="display-3 nexomon-header text-center">{nexomonData.name}</h1>
-                <img src={nexomonData.imgUrl} alt={nexomonData.name}/>
-                <br />{nexomonData.nexomonType}<img src={nexomonData.typeUrl} alt={nexomonData.nexomonType}/>
+                {/* <h1 className="display-3 nexomon-header text-center">{nexomonData.name}</h1>
+                <img className="float-left" src={nexomonData.imgUrl} alt={nexomonData.name}/>
+                <span>{nexomonData.number}</span>
+                <br />{nexomonData.nexomonType}<img src={nexomonData.typeUrl} alt={nexomonData.nexomonType}/> */}
+                
+                <div className="nexomon-header">
+                <Row>
+                    <Col><h1 className="display-3 text-center">{nexomonData.name} <span>#{nexomonData.number}</span></h1></Col>                    
+                </Row>
+                </div>
+                <Row>
+                    <Col><img className="nexomon-sprite-img float-left" src={nexomonData.imgUrl} alt={nexomonData.name}/>
+                    <span className="align-top text-center">{nexomonData.nexomonType}<img src={nexomonData.typeUrl} className="nexomon-element-img" alt={nexomonData.nexomonType}/></span>
+                    </Col>
+                </Row>
             </Container>
         </div>
     )
